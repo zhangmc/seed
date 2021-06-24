6382e7c09eb6b3ad8d58b4cdb31b5190047d04cb
ZMC <zmc0927@gmail.com>
2021-06-24 11:11:15 +0800

    update

--- src/main/java/com/company/project/configurer/MybatisConfigurer.java
+++ src/main/java/com/company/project/configurer/MybatisConfigurer.java
@@ -1,19 +1,15 @@
 package com.company.project.configurer;
 
-import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInterceptor;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 import tk.mybatis.spring.mapper.MapperScannerConfigurer;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.util.Properties;
 
@@ -31,25 +27,33 @@ public class MybatisConfigurer {
         factory.setDataSource(dataSource);
         factory.setTypeAliasesPackage(MODEL_PACKAGE);
 
-        //配置分页插件，详情请查阅官方文档
-        PageHelper pageHelper = new PageHelper();
+        //配置分页插件，详情请查阅官方文档(PageHelper升级PageInterceptor)
+        PageInterceptor pageInterceptor = new PageInterceptor();
         Properties properties = new Properties();
         properties.setProperty("pageSizeZero", "true");//分页尺寸为0时查询所有纪录不再执行分页
         properties.setProperty("reasonable", "true");//页码<=0 查询第一页，页码>=总页数查询最后一页
         properties.setProperty("supportMethodsArguments", "true");//支持通过 Mapper 接口参数来传递分页参数
-        pageHelper.setProperties(properties);
+        pageInterceptor.setProperties(properties);
 
         //添加插件
-        factory.setPlugins(new Interceptor[]{pageHelper});
+        factory.setPlugins(new Interceptor[]{pageInterceptor});
 
         //添加XML目录
         ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        factory.setMapperLocations(resolver.getResources("classpath:mapper/*.xml"));
+        factory.setMapperLocations(resolver.getResources("classpath*:mapper/*.xml"));
         return factory.getObject();
     }
 
     @Bean
-    public MapperScannerConfigurer mapperScannerConfigurer() {
+    /**
+     * <bean class="tk.mybatis.spring.mapper.MapperScannerConfigurer">
+     *  <property name="mappers" value="com.company.project.core.ProjectConstant.MAPPER_INTERFACE_REFERENCE" />
+     *  <property name="notEmpty" value="false" />
+     *  <property name="IDENTITY" value="MYSQL" />
+     *  <property name="sqlSessionFactoryBeanName" value="sqlSessionFactory" />
+     *  </bean>
+     */
+    public static MapperScannerConfigurer mapperScannerConfigurer() {
         MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
         mapperScannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactoryBean");
         mapperScannerConfigurer.setBasePackage(MAPPER_PACKAGE);
